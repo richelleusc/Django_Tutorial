95e05eb5560c473f61333192c440ad5e89831d0f
Richel Leung <>
2019-09-16 11:28:20 -0700

    feat(Django Tutorial): Part 3
    Writing views, 404 Errors, and using the template system
    
    [ICTRAINING-252]

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
+        # Ex: /polls/
+        path('', views.index, name='index'),
+        # Ex: /polls/5/
+        path('<int:question_id>/', views.detail, name='detail'),
+        # Ex: /polls/5/results
+        path('<int:question_id>/results/', views.results, name='results'),
+        # Ex: /polls/5/vote/
+        path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
