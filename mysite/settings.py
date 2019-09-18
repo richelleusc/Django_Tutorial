531dd365378fcf1b653e83ee189dd81fb1fdaf7e
Richel Leung <>
2019-09-18 14:15:55 -0700

    fix(Django Tutorial): Heroku Static Assets
    Add django-app configuration
    
    [ICTRAINING-258]

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,4 +135,6 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
